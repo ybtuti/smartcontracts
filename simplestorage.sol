98b82fe79654e1e9e9a1749c5d4c7f505dcdc73f
sam <samuelmsuu@gmail.com>
2024-04-12 20:44:16 +0300

    added a function

--- simplestorage.sol
+++ simplestorage.sol
@@ -8,12 +8,17 @@ contract simplestorage {
         uint256 favouriteNumber;
         string name;
     }
-    People public person = People({favouriteNumber: 2, name: "patrick"});
+    People[] public people;
+    mapping(string => uint256) public nameToFavouriteNumber;
     function store(uint256 _favouriteNumber) public{
         favoriteNumber = _favouriteNumber;
     }
     //view , pure are used for reading the state of the blockchain
     function retrieve() public view returns(uint256){
         return favoriteNumber;
+    }
+    function addPerson(string memory _name, uint256 _favouriteNumber)public{
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
     }
 }
